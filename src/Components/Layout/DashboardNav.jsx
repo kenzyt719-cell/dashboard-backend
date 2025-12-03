380174244d1c139abe1a23be683b8dcf6e8fa74b
Kenzy Tarek <kenzy@Kenzys-MacBook-Pro.local>
2025-12-03 22:22:58 +0200

    fix:HOMEV PAGE

--- src/Components/Layout/DashboardNav.jsx
+++ src/Components/Layout/DashboardNav.jsx
@@ -3,9 +3,6 @@ import "./DashboardNav.css";
 import logo from "../../Assets/main logo nav.png";
 import NavItem from "../Common/NavItem";
 
-
-
-// icons
 import icon1 from "../../Assets/icon1.svg";
 import icon2 from "../../Assets/icon2.svg";
 import icon3 from "../../Assets/icon3.svg";
@@ -16,59 +13,69 @@ import icon7 from "../../Assets/icon7.svg";
 import icon8 from "../../Assets/icon8.svg";
 
 const DashboardNav = () => {
+  // حالة فتح وغلق القائمة بالكامل (للشاشات الصغيرة)
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // حالة فتح وغلق القوائم الفرعية
   const [openProjects, setOpenProjects] = useState(false);
   const [openInbox, setOpenInbox] = useState(false);
 
   return (
-    <nav className="dashboard-nav">
-      <img src={logo} alt="Main Logo" className="logo" />
+    <>
+      {/* Top Bar Mobile */}
+      <div className="mobile-topbar">
+        <img src={logo} alt="logo" className="logo" />
+        <div className="burger" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+      </div>
 
-      <ul>
-        <NavItem icon={icon1} label="Dashboard" />
+      {/* Sidebar */}
+      <nav className={`dashboard-nav ${menuOpen ? "open" : "closed"}`}>
+        <ul>
+          <NavItem icon={icon1} label="Dashboard" />
 
-        <NavItem
-          icon={icon2}
-          label="Projects ▾"
-          onClick={() => setOpenProjects(!openProjects)}
-        >
-          {openProjects && (
-            <>
-              <li>UX/UI</li>
-              <li>Graphic Design</li>
-              <li>Front End</li>
-              <li>Photography</li>
-              <li>Filmmaking</li>
-              <li>Branding</li>
-              <li>3D</li>
-            </>
-          )}
-        </NavItem>
+          <NavItem
+            icon={icon2}
+            label="Projects ▾"
+            onClick={() => setOpenProjects(!openProjects)}
+          >
+            {openProjects && (
+              <>
+                <li>UX/UI</li>
+                <li>Graphic Design</li>
+                <li>Front End</li>
+                <li>Photography</li>
+                <li>Filmmaking</li>
+                <li>Branding</li>
+                <li>3D</li>
+              </>
+            )}
+          </NavItem>
 
-        <NavItem icon={icon3} label="Categories" />
+          <NavItem icon={icon3} label="Categories" />
+          <NavItem icon={icon4} label="Pages" />
+          <NavItem icon={icon5} label="Skills" />
 
-        <NavItem icon={icon4} label="Pages" />
+          <NavItem
+            icon={icon6}
+            label="Inbox ▾"
+            onClick={() => setOpenInbox(!openInbox)}
+          >
+            {openInbox && (
+              <>
+                <li>Messages</li>
+                <li>Favorites</li>
+                <li>Deleted</li>
+              </>
+            )}
+          </NavItem>
 
-        <NavItem icon={icon5} label="Skills" />
-
-        <NavItem
-          icon={icon6}
-          label="Inbox ▾"
-          onClick={() => setOpenInbox(!openInbox)}
-        >
-          {openInbox && (
-            <>
-              <li>Messages</li>
-              <li>Favorites</li>
-              <li>Deleted</li>
-            </>
-          )}
-        </NavItem>
-
-        <NavItem icon={icon7} label="Profile" />
-
-        <NavItem icon={icon8} label="Log Out" />
-      </ul>
-    </nav>
+          <NavItem icon={icon7} label="Profile" />
+          <NavItem icon={icon8} label="Log Out" />
+        </ul>
+      </nav>
+    </>
   );
 };
 
